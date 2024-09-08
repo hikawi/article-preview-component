74bf4da2d52c7b3906df28d771e4606e9a5bbe75
tsukimi <128211112+hikawi@users.noreply.github.com>
2024-09-09 02:07:45 +0700

    bruh this is hard

--- src/components/AuthorDisplay.vue
+++ src/components/AuthorDisplay.vue
@@ -2,35 +2,35 @@
 import { ref } from "vue";
 import michelle from "./assets/avatar-michelle.jpg";
 import IconShare from "./icons/IconShare.vue";
-import MobileShareView from "./MobileShareView.vue";
 
 const active = ref(false);
 </script>
 
 <template>
-  <div class="relative mt-2 flex w-full flex-row items-center justify-between">
-    <div class="flex h-full flex-row gap-4">
+  <div
+    class="relative flex h-16 w-full flex-row items-center justify-between px-10 pb-6"
+  >
+    <div class="flex h-full flex-row items-center justify-center gap-4">
       <img
         :src="michelle.src"
         alt="Avatar of the author, Michelle"
         :width="40"
         :height="40"
-        class="rounded-full"
+        class="rounded-full object-contain"
       />
-      <div class="flex h-full flex-col">
+      <div class="flex h-full flex-col items-start justify-center">
         <p class="text-dark-gray-blue text-sm font-bold">Michelle Appleton</p>
         <p class="text-desat-dark-blue text-sm font-medium">28 Jun 2020</p>
       </div>
     </div>
-    <button
-      class="bg-light-gray-blue flex size-8 items-center justify-center rounded-full"
+    <div
+      class="flex size-8 cursor-pointer items-center justify-center rounded-full"
+      :class="{ 'bg-desat-dark-blue': active, 'bg-light-gray-blue': !active }"
       @click="active = !active"
     >
       <IconShare
         :class="{ 'fill-white': active, 'fill-desat-dark-blue': !active }"
       />
-    </button>
-
-    <MobileShareView v-show="active" @update="active = false" />
+    </div>
   </div>
 </template>
